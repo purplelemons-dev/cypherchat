9e7edbbb5c8cc101f770603662b1545f22e7f35f
purplelemons-dev <ironbreaker2005@gmail.com>
2023-11-10 01:20:05 -0600

    bullshit thats actually for #3

--- src/lib/LoginForm.svelte
+++ src/lib/LoginForm.svelte
@@ -1,12 +1,19 @@
 <script>
 	export let email = '';
 	export let password = '';
+	export let username = '';
 </script>
 
 <p>
 	<label>
 		Email:
 		<input type="email" bind:value={email} placeholder="you@example.com" required />
+	</label>
+</p>
+<p>
+	<label>
+		Username:
+		<input type="text" bind:value={username} placeholder="username" required />
 	</label>
 </p>
 <p>
---
+++ src/routes/me/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { SignedIn } from 'sveltefire';
+	import { firestore } from '$lib';
+	import Messenger from '$lib/Messenger.svelte';
+</script>
+
+<SignedIn>
+	<!-- TODO: messaging interface -->
+	<Messenger {firestore} />
+</SignedIn>
